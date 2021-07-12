e2a4c3b891062445bd7958dfb18821e03ef544c4
Stefan Bohacek <stefan@fourtonfish.com>
2021-07-11 22:11:23 -0400

    Fixed issue with selected text not accepting further redacting.

--- README.md
+++ README.md
@@ -5,7 +5,3 @@
 Redact text on a web page before taking a screenshot using the [Redacted](https://github.com/christiannaths/Redacted-Font) font created by [Christian Naths](https://christiannaths.com/).
 
 Coming soon to Chrome and Firefox.
-
-## Known issues
-
-- After redacting selected text, it is not possible to apply different style to the same text until the page is refreshed.
--- browser-extension/scripts/content.js
+++ browser-extension/scripts/content.js
@@ -1,5 +1,10 @@
+function redactAll( request ){
+  Array.prototype.map.call( document.querySelectorAll( '*' ), function( curr ){
+    curr.style.fontFamily = request.font;
+  } );
+}
+
 chrome.runtime.onMessage.addListener( function( request ){
-  // console.log( request );
   if ( request && request.style && request.font ){
     let headElement = document.getElementsByTagName("head")[0],
         linkElement = document.createElement("link");
@@ -11,19 +16,33 @@ chrome.runtime.onMessage.addListener( function( request ){
             + '"); }';
 
     document.head.appendChild( styles );
-    // console.log( chrome.extension.getURL( `fonts/redacted-${ request.style }.woff` ) )
-    let selection = window.getSelection();
-    if ( selection && selection.rangeCount ){
-      selection = selection.getRangeAt( 0 );
-      var selectedText = selection.extractContents();
-      var span = document.createElement( "span" );
-      span.style.fontFamily = request.font;
-      span.appendChild( selectedText );
-      selection.insertNode( span );
-    } else {
-      Array.prototype.map.call( document.querySelectorAll( '*' ), function( curr ){
-        curr.style.fontFamily = request.font;
-      } );
+
+    try{
+      const selectedText = window.getSelection().toString();
+      if ( selectedText && selectedText.length ){
+        let selection = window.getSelection();
+        selection = selection.getRangeAt( 0 );
+
+        let selectedText = selection.extractContents();
+        let span = document.createElement( 'span' );
+
+        span.style.fontFamily = request.font;
+        span.appendChild( selectedText );
+        selection.insertNode( span );
+        let innerSpans = span.querySelectorAll( 'span' );
+        if ( innerSpans && innerSpans.length > 0 ){
+          for ( let i = 0, j = innerSpans.length; i < j; i++ ){
+            if ( innerSpans[i].getAttribute('style') && innerSpans[i].getAttribute('style').indexOf( 'Redacted' ) !== -1 ){
+              innerSpans[i].style = '';
+            }
+          }
+        }
+      } else {
+        redactAll( request );
+      }
+    } catch( err ){
+      console.log( 'Redacted error:', err );
+      redactAll( request );
     }
   }
 } );
